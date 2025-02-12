c3eb93e0e3fb1808a39083387a22af3b57ce2e4e
yanis <meriem.yanis05@gmail.com>
2025-02-12 10:44:51 +0100

    mise en place de la hiérarchi des fichier

---
+++ Assets/Scripte/ManagerDoor.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class ManagerDoor : MonoBehaviour
+{
+    public Animator anim;
+    public bool isOpen;
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripte/Recepteur.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Recepteur : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
