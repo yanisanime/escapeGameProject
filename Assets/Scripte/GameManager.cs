7ba25679775dbdf1f6d3cce9b9dd0b1e69261e46
yanis <meriem.yanis05@gmail.com>
2025-02-18 12:28:36 +0100

    le jeux est NORMALEMENT fonctionnel

---
+++ Assets/Scripte/GameManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+
+
+
+    //fonction pour recharger la scene
+    public void ReloadScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
